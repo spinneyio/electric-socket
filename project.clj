7fdace7dfcaace470891f821547afb79a7bf0540
Marek Lipert <marek@spinney.io>
2022-08-12 11:46:43 +1200

    Fix project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject electric-socket "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library designed to bring order to chaotic world of socket communication."
+  :url "https://github.com/spinneyio/electric-socket"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
